44382087bb3af4c0ff47cbfc8b6826d0ab06566f
auduongtansang <1712145@student.hcmus.edu.vn>
2019-10-07 14:22:08 +0700

    First commit (upload files only)

---
+++ TheHandsGL/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TheHandsGL
+{
+	partial class mainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.drawBoard = new SharpGL.OpenGLControl();
+			this.colorDialog = new System.Windows.Forms.ColorDialog();
+			this.btnLine = new System.Windows.Forms.Button();
+			this.btnColor = new System.Windows.Forms.Button();
+			this.btnWidth = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.drawBoard)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// drawBoard
+			// 
+			this.drawBoard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.drawBoard.DrawFPS = false;
+			this.drawBoard.Location = new System.Drawing.Point(35, 0);
+			this.drawBoard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.drawBoard.Name = "drawBoard";
+			this.drawBoard.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+			this.drawBoard.RenderContextType = SharpGL.RenderContextType.DIBSection;
+			this.drawBoard.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+			this.drawBoard.Size = new System.Drawing.Size(800, 600);
+			this.drawBoard.TabIndex = 0;
+			this.drawBoard.OpenGLInitialized += new System.EventHandler(this.drawBoard_OpenGLInitialized);
+			this.drawBoard.OpenGLDraw += new SharpGL.RenderEventHandler(this.drawBoard_OpenGLDraw);
+			this.drawBoard.Resized += new System.EventHandler(this.drawBoard_Resized);
+			this.drawBoard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawBoard_MouseDown);
+			this.drawBoard.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawBoard_MouseMove);
+			this.drawBoard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawBoard_MouseUp);
+			// 
+			// btnLine
+			// 
+			this.btnLine.Location = new System.Drawing.Point(0, 0);
+			this.btnLine.Name = "btnLine";
+			this.btnLine.Size = new System.Drawing.Size(35, 35);
+			this.btnLine.TabIndex = 1;
+			this.btnLine.Text = "Li";
+			this.btnLine.UseVisualStyleBackColor = true;
+			this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+			// 
+			// btnColor
+			// 
+			this.btnColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnColor.Location = new System.Drawing.Point(0, 565);
+			this.btnColor.Name = "btnColor";
+			this.btnColor.Size = new System.Drawing.Size(35, 35);
+			this.btnColor.TabIndex = 2;
+			this.btnColor.Text = "CL";
+			this.btnColor.UseVisualStyleBackColor = true;
+			this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+			// 
+			// btnWidth
+			// 
+			this.btnWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+			this.btnWidth.Location = new System.Drawing.Point(0, 530);
+			this.btnWidth.Name = "btnWidth";
+			this.btnWidth.Size = new System.Drawing.Size(35, 35);
+			this.btnWidth.TabIndex = 3;
+			this.btnWidth.Text = "2.0";
+			this.btnWidth.UseVisualStyleBackColor = true;
+			this.btnWidth.Click += new System.EventHandler(this.btnWidth_Click);
+			// 
+			// mainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(835, 600);
+			this.Controls.Add(this.btnWidth);
+			this.Controls.Add(this.btnColor);
+			this.Controls.Add(this.btnLine);
+			this.Controls.Add(this.drawBoard);
+			this.MinimumSize = new System.Drawing.Size(835, 600);
+			this.Name = "mainForm";
+			this.Text = "TheHandsGL";
+			((System.ComponentModel.ISupportInitialize)(this.drawBoard)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private SharpGL.OpenGLControl drawBoard;
+		private System.Windows.Forms.ColorDialog colorDialog;
+		private System.Windows.Forms.Button btnLine;
+		private System.Windows.Forms.Button btnColor;
+		private System.Windows.Forms.Button btnWidth;
+	}
+}
+
---
+++ TheHandsGL/Form1.cs
@@ -0,0 +1,208 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheHandsGL
+{
+	public partial class mainForm : Form
+	{
+		//Biến đánh dấu rằng người dùng đang vẽ hình (quá trình vẽ bắt đầu khi nhấn giữ chuột và kéo đi, kết thúc khi nhả chuột)
+		bool isDrawing = false;
+		//Màu người dùng đang chọn
+		Color userColor = Color.Black;
+		//Độ dày nét vẽ người dùng đang chọn
+		float userWidth = 2.0f;
+		//Loại hình vẽ người dùng đang chọn
+		Shape.shapeType userType = Shape.shapeType.NONE;
+		//Tọa độ chuột
+		Point pStart = new Point(0, 0), pEnd = new Point(0, 0);
+		//Danh sách các hình đã vẽ
+		List<Shape> shapes = new List<Shape>();
+		//Biến đánh dấu danh sách hình vẽ bị thay đổi, cần phải vẽ lại
+		bool isShapesChanged = true;
+
+		public mainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void drawBoard_OpenGLInitialized(object sender, EventArgs e)
+		{
+			//Sự kiện "khởi tạo", xảy ra khi chương trình vừa được khởi chạy
+
+			//Lấy đối tượng OpenGL
+			OpenGL gl = drawBoard.OpenGL;
+
+			//Set màu nền (trắng)
+			gl.ClearColor(1, 1, 1, 1);
+
+			//Xóa toàn bộ drawBoard
+			gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+		}
+
+		private void drawBoard_Resized(object sender, EventArgs e)
+		{
+			//Sự kiện "thay đổi kích thước cửa sổ"
+
+			//Lấy đối tượng OpenGL
+			OpenGL gl = drawBoard.OpenGL;
+
+			//Chọn chế độ chiếu
+			gl.MatrixMode(OpenGL.GL_PROJECTION);
+			//Chọn ma trận chiếu là mà trận đơn vị
+			gl.LoadIdentity();
+			//Set viewport theo kích thước mới
+			gl.Viewport(0, 0, drawBoard.Width, drawBoard.Height);
+			//Chiếu lên viewport này
+			gl.Ortho2D(0, drawBoard.Width, 0, drawBoard.Height);
+			//Set biến đánh dấu để vẽ lại hình
+			isShapesChanged = true;
+		}
+
+		private void drawBoard_OpenGLDraw(object sender, RenderEventArgs args)
+		{
+			//Sự kiện "vẽ", xảy ra liên tục và lặp vô hạn lần
+			
+			if (isDrawing == false && isShapesChanged == true)
+			{
+				//Lấy đối tượng OpenGL
+				OpenGL gl = drawBoard.OpenGL;
+
+				//Xóa toàn bộ drawBoard
+				gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+				//Vẽ lại tất cả hình
+				foreach (Shape shape in shapes)
+					shape.draw(gl);
+
+				gl.Flush();
+				isShapesChanged = false;
+			}
+		}
+
+		private void drawBoard_MouseDown(object sender, MouseEventArgs e)
+		{
+			//Sự kiện "nhấn giữ chuột", bắt đầu quá trình vẽ
+			pStart = pEnd = e.Location;
+			isDrawing = true;
+		}
+
+		private void drawBoard_MouseUp(object sender, MouseEventArgs e)
+		{
+			//Sự kiện "nhả chuột", kết thúc quá trình vẽ
+			pEnd = e.Location;
+			isDrawing = false;
+
+			//Tạo hình vẽ mới và thêm vào danh sách, đánh dấu danh sách đã bị thay đổi
+			Shape newShape = new Shape(userColor, userWidth, userType);
+			switch (userType)
+			{
+				case Shape.shapeType.LINE:
+					newShape.points.Add(pStart);
+					newShape.points.Add(pEnd);
+					break;
+			}
+			shapes.Add(newShape);
+			isShapesChanged = true;
+		}
+
+		private void drawBoard_MouseMove(object sender, MouseEventArgs e)
+		{
+			//Sự kiện "nhấn giữ chuột và kéo"
+			if (isDrawing)
+			{
+				pEnd = e.Location;
+
+				//Lấy đối tượng OpenGL
+				OpenGL gl = drawBoard.OpenGL;
+
+				//Xóa toàn bộ drawBoard
+				gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+				//Vẽ lại tất cả hình
+				foreach (Shape shape in shapes)
+					shape.draw(gl);
+
+				//Tạo hình vẽ mới và vẽ hình này ra (không thêm hình này vào list vì chưa vẽ xong)
+				Shape newShape = new Shape(userColor, userWidth, userType);
+				switch (userType)
+				{
+					case Shape.shapeType.LINE:
+						newShape.points.Add(pStart);
+						newShape.points.Add(pEnd);
+						break;
+				}
+				newShape.draw(gl);
+				gl.Flush();
+			}
+		}
+
+		private void btnLine_Click(object sender, EventArgs e)
+		{
+			//Sự kiện "chọn vẽ đường thẳng"
+			userType = Shape.shapeType.LINE;
+		}
+
+		private void btnWidth_Click(object sender, EventArgs e)
+		{
+			//Sự kiện "thay đổi độ dày nét vẽ"
+			userWidth += 0.5f;
+			if (userWidth > 5)
+				userWidth = 1.0f;
+			btnWidth.Text = userWidth.ToString("0.0");
+		}
+
+		private void btnColor_Click(object sender, EventArgs e)
+		{
+			//Sự kiện "chọn màu"
+			if (colorDialog.ShowDialog() == DialogResult.OK)
+				userColor = colorDialog.Color;
+		}
+	}
+
+	class Shape
+	{
+		//Lớp "Shape", định nghĩa một hình vẽ
+		public enum shapeType { NONE, LINE, CIRCLE, RECTANGLE, ELLIPSE, TRIANGLE, PENTAGON, HEXAGON }
+
+		//Màu nét vẽ
+		public Color lineColor;
+		//Độ dày nét vẽ
+		public float lineWidth;
+		//Loại hình vẽ
+		public shapeType type;
+		//Danh sách các điểm neo
+		public List<Point> points;
+
+		public Shape(Color userColor, float userWidth, shapeType userType)
+		{
+			lineColor = userColor;
+			lineWidth = userWidth;
+			type = userType;
+			points = new List<Point>();
+		}
+
+		public void draw(OpenGL gl)
+		{
+			gl.LineWidth(lineWidth);
+			gl.Color(lineColor.R / 255, lineColor.G / 255, lineColor.B / 255);
+
+			if (type == shapeType.LINE)
+				gl.Begin(OpenGL.GL_LINES);
+			else
+				gl.Begin(OpenGL.GL_LINE_LOOP);
+
+			foreach (Point point in points)
+				gl.Vertex(point.X, gl.RenderContextProvider.Height - point.Y);
+
+			gl.End();
+		}
+	}
+}
