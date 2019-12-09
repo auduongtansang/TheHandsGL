b1fba4cc60ccb254ec962f06a6facc09990be33b
auduongtansang <1712145@student.hcmus.edu.vn>
2019-12-09 09:34:21 +0700

    UI minor update

--- TheHandsGL/MainForm.Designer.cs
+++ TheHandsGL/MainForm.Designer.cs
@@ -56,13 +56,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.drawBoard.Cursor = System.Windows.Forms.Cursors.Cross;
 			this.drawBoard.DrawFPS = false;
-			this.drawBoard.Location = new System.Drawing.Point(0, 46);
-			this.drawBoard.Margin = new System.Windows.Forms.Padding(5);
+			this.drawBoard.Location = new System.Drawing.Point(0, 37);
+			this.drawBoard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.drawBoard.Name = "drawBoard";
 			this.drawBoard.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
 			this.drawBoard.RenderContextType = SharpGL.RenderContextType.DIBSection;
 			this.drawBoard.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
-			this.drawBoard.Size = new System.Drawing.Size(891, 601);
+			this.drawBoard.Size = new System.Drawing.Size(668, 488);
 			this.drawBoard.TabIndex = 0;
 			this.drawBoard.OpenGLInitialized += new System.EventHandler(this.drawBoard_OpenGLInitialized);
 			this.drawBoard.OpenGLDraw += new SharpGL.RenderEventHandler(this.drawBoard_OpenGLDraw);
@@ -78,10 +78,10 @@
 			// 
 			// btnLine
 			// 
-			this.btnLine.Location = new System.Drawing.Point(0, 5);
-			this.btnLine.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnLine.Location = new System.Drawing.Point(0, 4);
+			this.btnLine.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnLine.Name = "btnLine";
-			this.btnLine.Size = new System.Drawing.Size(91, 34);
+			this.btnLine.Size = new System.Drawing.Size(68, 28);
 			this.btnLine.TabIndex = 1;
 			this.btnLine.Text = "Line";
 			this.btnLine.UseVisualStyleBackColor = true;
@@ -89,10 +89,10 @@
 			// 
 			// btnCircle
 			// 
-			this.btnCircle.Location = new System.Drawing.Point(91, 5);
-			this.btnCircle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnCircle.Location = new System.Drawing.Point(68, 4);
+			this.btnCircle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnCircle.Name = "btnCircle";
-			this.btnCircle.Size = new System.Drawing.Size(91, 34);
+			this.btnCircle.Size = new System.Drawing.Size(68, 28);
 			this.btnCircle.TabIndex = 4;
 			this.btnCircle.Text = "Circle";
 			this.btnCircle.UseVisualStyleBackColor = true;
@@ -100,10 +100,10 @@
 			// 
 			// btnRectangle
 			// 
-			this.btnRectangle.Location = new System.Drawing.Point(180, 5);
-			this.btnRectangle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnRectangle.Location = new System.Drawing.Point(135, 4);
+			this.btnRectangle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnRectangle.Name = "btnRectangle";
-			this.btnRectangle.Size = new System.Drawing.Size(91, 34);
+			this.btnRectangle.Size = new System.Drawing.Size(68, 28);
 			this.btnRectangle.TabIndex = 5;
 			this.btnRectangle.Text = "Rectangle";
 			this.btnRectangle.UseVisualStyleBackColor = true;
@@ -111,10 +111,10 @@
 			// 
 			// btnEllipse
 			// 
-			this.btnEllipse.Location = new System.Drawing.Point(269, 5);
-			this.btnEllipse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnEllipse.Location = new System.Drawing.Point(202, 4);
+			this.btnEllipse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnEllipse.Name = "btnEllipse";
-			this.btnEllipse.Size = new System.Drawing.Size(91, 34);
+			this.btnEllipse.Size = new System.Drawing.Size(68, 28);
 			this.btnEllipse.TabIndex = 6;
 			this.btnEllipse.Text = "Ellipse";
 			this.btnEllipse.UseVisualStyleBackColor = true;
@@ -122,10 +122,10 @@
 			// 
 			// btnTriangle
 			// 
-			this.btnTriangle.Location = new System.Drawing.Point(360, 5);
-			this.btnTriangle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnTriangle.Location = new System.Drawing.Point(270, 4);
+			this.btnTriangle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnTriangle.Name = "btnTriangle";
-			this.btnTriangle.Size = new System.Drawing.Size(91, 34);
+			this.btnTriangle.Size = new System.Drawing.Size(68, 28);
 			this.btnTriangle.TabIndex = 7;
 			this.btnTriangle.Text = "Triangle";
 			this.btnTriangle.UseVisualStyleBackColor = true;
@@ -133,10 +133,10 @@
 			// 
 			// btnPentagon
 			// 
-			this.btnPentagon.Location = new System.Drawing.Point(451, 5);
-			this.btnPentagon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnPentagon.Location = new System.Drawing.Point(338, 4);
+			this.btnPentagon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnPentagon.Name = "btnPentagon";
-			this.btnPentagon.Size = new System.Drawing.Size(91, 34);
+			this.btnPentagon.Size = new System.Drawing.Size(68, 28);
 			this.btnPentagon.TabIndex = 8;
 			this.btnPentagon.Text = "Pentagon";
 			this.btnPentagon.UseVisualStyleBackColor = true;
@@ -144,10 +144,10 @@
 			// 
 			// btnHexagon
 			// 
-			this.btnHexagon.Location = new System.Drawing.Point(540, 5);
-			this.btnHexagon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnHexagon.Location = new System.Drawing.Point(405, 4);
+			this.btnHexagon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnHexagon.Name = "btnHexagon";
-			this.btnHexagon.Size = new System.Drawing.Size(91, 34);
+			this.btnHexagon.Size = new System.Drawing.Size(68, 28);
 			this.btnHexagon.TabIndex = 9;
 			this.btnHexagon.Text = "Hexagon";
 			this.btnHexagon.UseVisualStyleBackColor = true;
@@ -155,10 +155,10 @@
 			// 
 			// btnPolygon
 			// 
-			this.btnPolygon.Location = new System.Drawing.Point(629, 5);
-			this.btnPolygon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnPolygon.Location = new System.Drawing.Point(472, 4);
+			this.btnPolygon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnPolygon.Name = "btnPolygon";
-			this.btnPolygon.Size = new System.Drawing.Size(91, 34);
+			this.btnPolygon.Size = new System.Drawing.Size(68, 28);
 			this.btnPolygon.TabIndex = 17;
 			this.btnPolygon.Text = "Polygon";
 			this.btnPolygon.UseVisualStyleBackColor = true;
@@ -167,10 +167,10 @@
 			// btnClear
 			// 
 			this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnClear.Location = new System.Drawing.Point(800, 5);
-			this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnClear.Location = new System.Drawing.Point(600, 4);
+			this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnClear.Name = "btnClear";
-			this.btnClear.Size = new System.Drawing.Size(91, 34);
+			this.btnClear.Size = new System.Drawing.Size(68, 28);
 			this.btnClear.TabIndex = 10;
 			this.btnClear.Text = "ClearAll";
 			this.btnClear.UseVisualStyleBackColor = true;
@@ -180,10 +180,10 @@
 			// 
 			this.btnWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.btnWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-			this.btnWidth.Location = new System.Drawing.Point(91, 650);
-			this.btnWidth.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnWidth.Location = new System.Drawing.Point(68, 528);
+			this.btnWidth.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnWidth.Name = "btnWidth";
-			this.btnWidth.Size = new System.Drawing.Size(91, 34);
+			this.btnWidth.Size = new System.Drawing.Size(68, 28);
 			this.btnWidth.TabIndex = 3;
 			this.btnWidth.Text = "Width: 1.0";
 			this.btnWidth.UseVisualStyleBackColor = true;
@@ -192,10 +192,10 @@
 			// btnColor
 			// 
 			this.btnColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.btnColor.Location = new System.Drawing.Point(0, 650);
-			this.btnColor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnColor.Location = new System.Drawing.Point(0, 528);
+			this.btnColor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnColor.Name = "btnColor";
-			this.btnColor.Size = new System.Drawing.Size(91, 34);
+			this.btnColor.Size = new System.Drawing.Size(68, 28);
 			this.btnColor.TabIndex = 2;
 			this.btnColor.Text = "Color";
 			this.btnColor.UseVisualStyleBackColor = true;
@@ -204,9 +204,10 @@
 			// btnRotateOrScale
 			// 
 			this.btnRotateOrScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.btnRotateOrScale.Location = new System.Drawing.Point(182, 650);
+			this.btnRotateOrScale.Location = new System.Drawing.Point(136, 528);
+			this.btnRotateOrScale.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.btnRotateOrScale.Name = "btnRotateOrScale";
-			this.btnRotateOrScale.Size = new System.Drawing.Size(91, 34);
+			this.btnRotateOrScale.Size = new System.Drawing.Size(68, 28);
 			this.btnRotateOrScale.TabIndex = 19;
 			this.btnRotateOrScale.Text = "Rotate";
 			this.btnRotateOrScale.UseVisualStyleBackColor = true;
@@ -217,48 +218,51 @@
 			this.lbUndo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.lbUndo.AutoSize = true;
 			this.lbUndo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-			this.lbUndo.Location = new System.Drawing.Point(279, 661);
+			this.lbUndo.Location = new System.Drawing.Point(209, 537);
+			this.lbUndo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.lbUndo.Name = "lbUndo";
-			this.lbUndo.Size = new System.Drawing.Size(96, 17);
+			this.lbUndo.Size = new System.Drawing.Size(73, 13);
 			this.lbUndo.TabIndex = 11;
 			this.lbUndo.Text = "Undo: Ctrl + Z";
-			// 
-			// lbMode
-			// 
-			this.lbMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.lbMode.AutoSize = true;
-			this.lbMode.Location = new System.Drawing.Point(381, 661);
-			this.lbMode.Name = "lbMode";
-			this.lbMode.Size = new System.Drawing.Size(96, 17);
-			this.lbMode.TabIndex = 18;
-			this.lbMode.Text = "Mode: Picking";
 			// 
 			// lbTime
 			// 
 			this.lbTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.lbTime.AutoSize = true;
 			this.lbTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
-			this.lbTime.Location = new System.Drawing.Point(751, 661);
+			this.lbTime.Location = new System.Drawing.Point(563, 537);
+			this.lbTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.lbTime.Name = "lbTime";
-			this.lbTime.Size = new System.Drawing.Size(43, 17);
+			this.lbTime.Size = new System.Drawing.Size(33, 13);
 			this.lbTime.TabIndex = 12;
 			this.lbTime.Text = "Time:";
 			// 
 			// tbTime
 			// 
 			this.tbTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.tbTime.Location = new System.Drawing.Point(800, 658);
-			this.tbTime.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.tbTime.Location = new System.Drawing.Point(597, 534);
+			this.tbTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.tbTime.Name = "tbTime";
 			this.tbTime.ReadOnly = true;
-			this.tbTime.Size = new System.Drawing.Size(79, 22);
+			this.tbTime.Size = new System.Drawing.Size(67, 20);
 			this.tbTime.TabIndex = 14;
+			// 
+			// lbMode
+			// 
+			this.lbMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.lbMode.AutoSize = true;
+			this.lbMode.Location = new System.Drawing.Point(286, 537);
+			this.lbMode.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lbMode.Name = "lbMode";
+			this.lbMode.Size = new System.Drawing.Size(75, 13);
+			this.lbMode.TabIndex = 18;
+			this.lbMode.Text = "Mode: Picking";
 			// 
 			// mainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(891, 690);
+			this.ClientSize = new System.Drawing.Size(668, 561);
 			this.Controls.Add(this.btnRotateOrScale);
 			this.Controls.Add(this.lbMode);
 			this.Controls.Add(this.btnPolygon);
@@ -276,8 +280,8 @@
 			this.Controls.Add(this.btnColor);
 			this.Controls.Add(this.btnLine);
 			this.Controls.Add(this.drawBoard);
-			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.MinimumSize = new System.Drawing.Size(890, 688);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.MinimumSize = new System.Drawing.Size(672, 566);
 			this.Name = "mainForm";
 			this.Text = "TheHandsGL";
 			((System.ComponentModel.ISupportInitialize)(this.drawBoard)).EndInit();
